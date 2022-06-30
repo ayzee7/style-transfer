aa379b175e55bb7d2b06f3eb41d519262f3e6f2a
ayzee7 <alex.rozen.5@gmail.com>
2022-06-30 15:27:11 +0300

    Added InlineKeyboardMarkup, option to change style transfer grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 import os
+import datetime as dt
 from telebot import types
 import logging
 from flask import Flask, request
@@ -23,22 +24,26 @@ bot = telebot.TeleBot(TOKEN)
 
 server = Flask(__name__)
 
+class Grade:
+    grade = 1         # defines morph grade
 
-showup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-showup.add(types.KeyboardButton('/transfer'))
-# stop = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-# stop.add(types.KeyboardButton('/cancel'))
+markup = types.InlineKeyboardMarkup()
+button_transfer = types.InlineKeyboardButton("Transfer", callback_data="tr")
+button_morph = types.InlineKeyboardButton("Change morph grade", callback_data="mg")
+markup.row(button_transfer)
+markup.row(button_morph)
+remove = types.ReplyKeyboardRemove(selective=False)
 
 @bot.message_handler(commands=['start'])
 def hello(message):
     bot.send_message(message.chat.id, 'Hello! Welcome to Style Transfer Bot. '
-                                      'Type in /transfer to start the bot.', reply_markup=showup)
+                                      'Type in /transfer to start the bot.', reply_markup=markup)
 
-
-@bot.message_handler(commands=['transfer'])
+@bot.message_handler()
 def style_in(message):
-    msg = bot.reply_to(message, 'Send me a photo to copy the style from')
-    bot.register_next_step_handler(msg, handle_style_photo)
+     if message.text == '/transfer':
+        msg = bot.reply_to(message, 'Send me a photo to copy the style from', reply_markup=remove)
+        bot.register_next_step_handler(msg, handle_style_photo)
 
 
 def handle_style_photo(message):
@@ -51,7 +56,7 @@ def handle_style_photo(message):
         msg = bot.reply_to(message, 'Send me a photo to copy the content from')
         bot.register_next_step_handler(msg, handle_content_photo)
     except TypeError:
-        bot.reply_to(message, 'You have to send a photo, not a text. Try again with /transfer', reply_markup=showup)
+        bot.reply_to(message, 'You have to send a photo. Try again with /transfer', reply_markup=markup)
         return None
 
 
@@ -64,28 +69,63 @@ def handle_content_photo(message):
             content.write(downloaded_file)
         bot.reply_to(message, 'Processing...') #, reply_markup=stop)
     except TypeError:
-        bot.reply_to(message, 'You have to send a photo, not a text. Try again with /transfer', reply_markup=showup)
+        bot.reply_to(message, 'You have to send a photo. Try again with /transfer', reply_markup=markup)
         return None
     try:
-        transfer()
+        transfer(Grade.grade)
         src = './data/images/neural-style/result.jpg'
         res = open(src, 'rb')
         bot.send_photo(message.chat.id, res)
         res.close()
         os.remove(src)
-        bot.send_message(message.chat.id, 'Wanna try one more time?', reply_markup=showup)
-    except AssertionError:
-        bot.send_message(message.chat.id, 'Sorry, the provided photos cannot be used together due to their size. Please try the other ones')
+        bot.send_message(message.chat.id, 'Wanna try one more time?', reply_markup=markup)
+    except SystemError:
+        bot.reply_to(message, 'Sorry, picture size is too big for bot to handle. Please, try another one', reply_markup=markup)
     except IOError:
         return None
 
+@bot.message_handler()
+def change_morph(message):
+    if message.text == '/morph':
+        msg = bot.reply_to(message, 'Type in the number from 0 to 1, which defines style transfer grade', reply_markup=remove)
+        bot.register_next_step_handler(msg, morph_reply)
 
-"""@bot.message_handler(commands=['cancel'])
-def cancel_message(message):
-    bot.send_message(message.chat.id, 'Stopping the bot...', reply_markup=showup)
-    src = './data/images/neural-style/.stop'
-    with open(src, 'w') as stop:
-        stop.write('1')"""
+def morph_reply(message):
+    try:
+        Grade.grade = float(message.text)
+        bot.reply_to(message, f"Style transfer grade was successfully changed to {Grade.grade}", reply_markup=markup)
+    except Exception:
+        bot.reply_to(message, 'Input is incorrect. Try again', reply_markup=markup)
+
+
+def send_transfer(message):
+    new_message = telebot.types.Message(message_id=message.message_id,
+                                        chat=message.chat,
+                                        content_type=["text"],
+                                        date=dt.datetime.today().timestamp(),
+                                        from_user=message.chat,
+                                        options={},
+                                        json_string="")
+    new_message.text = '/transfer'
+    style_in(new_message)
+
+def send_morph(message):
+    new_message = telebot.types.Message(message_id=message.message_id,
+                                        chat=message.chat,
+                                        content_type=["text"],
+                                        date=dt.datetime.today().timestamp(),
+                                        from_user=message.chat,
+                                        options={},
+                                        json_string="")
+    new_message.text = '/morph'
+    change_morph(new_message)
+
+@bot.callback_query_handler(lambda call: True)
+def style_in_call(call):
+    if call.data == "tr":
+        send_transfer(call.message)
+    elif call.data == "mg":
+        send_morph(call.message)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
